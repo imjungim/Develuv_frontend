65e6a23b631ce99debbf58b8da371a13b850a9f9
Kimseonghyoek <cilo1223@naver.com>
2022-07-05 01:04:30 +0900

    Add router function

--- develuv-f/src/App.js
+++ develuv-f/src/App.js
@@ -7,29 +7,30 @@ import EventSearch from "./Components/EventSearch";
 import EventItem from "./Components/EventItem";
 import Footer from "./Components/Footer";
 import Mainpage from "./Page/Mainpage";
-import EventInfo from './Containers/EventInfoContainer'
+import EventInfo from "./Containers/EventInfoContainer";
+import "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Login from "./Page/Login";
+
 const App = () => {
   return (
-    // <BrowserRouter>
-      <div className="App">
-        <Header />
-        {/*<Routes>
-          url경로와 컴포넌트를 맵핑합니다.
-          <Route path="/" element={<Mainpage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/explore" element={<EventList />} />
+    <div className="App">
+      <Header />
+      <Routes>
+        {/* url경로와 컴포넌트를 맵핑합니다. */}
+        <Route path="/" element={<Mainpage />} />
+        <Route path="/login" element={<Login />} />
+        {/* <Route path="/explore" element={<EventList />} />
           <Route path="/events" element={<EventInfoContainer />} />
           <Route path="/explore" element={<join />} />
           <Route path="/my/tickets" element={<MyTickets/>} />
           <Route path="/my/likes" element={<MyLikes/>} />
           <Route path="/my/events" element={<Myevent/>} />
           <Route path="/my/profile" element={<Myprofile/>} />
-        </Routes>*/}
-        <Mainpage/>
-      </div>
-   // </BrowserRouter>
+          */}
+      </Routes>
+    </div>
   );
 };
-
 
 export default App;
---
+++ develuv-f/src/Page/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from '../Components/Footer';
+
+const Login = () => {
+  return (
+    <div>
+      <h1>This is Login page</h1>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Login;
