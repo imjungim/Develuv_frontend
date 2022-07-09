9c48719bd0557eb53966f67f2e61a257496e6956
SungHyunGimHae <chh127000@naver.com>
2022-07-09 19:07:02 +0900

    Add: App.js file redux store

--- develuv-f/src/App.js
+++ develuv-f/src/App.js
@@ -12,24 +12,33 @@ import Explore from "./Page/Explore";
 import EventInfo from "./Containers/EventInfoContainer";
 import Myprofile from "./Page/Myprofile";
 import Codeofconduct from "./Page/code-of-conduct";
+import EventInfo from "./Containers/EventInfoContainer";
+
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './Modules/index';
+
+const store = createStore(rootReducer); // 스토어를 만듭니다.
+
 
 const App = () => {
   return (
     <div className="App">
       <Header />
-      <Routes>
-        {/* url경로와 컴포넌트를 맵핑합니다. */}
-        {/* <Route path="/" element={<Header />} /> */}
-        <Route path="/" element={<Mainpage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/explore" element={<Explore />} />
-        <Route path="/EventInfo" element={<EventInfo />} />
-        <Route path="/faq" element={<Faq />} />
-        <Route path="/code-of-conduct" element={<code-of-conduct/>}/>
-        <Route path="/myprofile" element={<Myprofile/>}/>
-      
-      </Routes>
-      <Footer/>
+      <Provider store={store}>
+        <Routes>
+          {/* url경로와 컴포넌트를 맵핑합니다. */}
+          {/* <Route path="/" element={<Header />} /> */}
+          <Route path="/" element={<Mainpage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/EventInfo" element={<EventInfo />} />
+          <Route path="/faq" element={<Faq />} />
+          <Route path="/code-of-conduct" element={<code-of-conduct />} />
+          <Route path="/myprofile" element={<Myprofile />} />
+        </Routes>
+      </Provider>
+      <Footer />
     </div>
   );
 };
