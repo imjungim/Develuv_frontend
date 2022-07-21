6eb453fdd78288aaef78a0a51dddde19f9b84001
SungHyunGimHae <chh127000@naver.com>
2022-07-21 14:26:41 +0900

    임시 수정

--- develuv-f/src/Components/EventCreate/EventCreateContent.js
+++ develuv-f/src/Components/EventCreate/EventCreateContent.js
@@ -2,51 +2,55 @@ import React from "react";
 import './EventCreateContent.scss'
 import { useState } from 'react';
 
-const EventCreateContent = ({ eventArticle, onChange,setEventArticle,onClick }) => {
+const EventCreateContent = ({ eventArticle, onChange, setEventArticle, onClick, }) => {
     const [imageSrc, setImageSrc] = useState('');
-
+ 
     const encodeFileToBase64 = (fileBlob) => {
         const reader = new FileReader();
         reader.readAsDataURL(fileBlob);
         return new Promise((resolve) => {
             reader.onload = () => {
                 setImageSrc(reader.result)
-                console.log(reader)
-                setEventArticle({
-                    ...eventArticle,
-                    img:reader.result
-                });
                 resolve();
             };
         });
     };
     return (
         <div id="createContent" >
-            <div id="eventTitle">
+            <form action='http://localhost:8081/EventCreate/upload'  method="post" enctype="multipart/form-data" id="createForm">
+                <div id="eventTitle">
 
-                <div className="createTitle">이벤트 제목</div>
-                <input name="title" className="createInput" placeholder="이벤트 제목" value={eventArticle.title} onChange={onChange} />
+                    <div className="createTitle">이벤트 제목</div>
+                    <input name="title" className="createInput" placeholder="이벤트 제목" value={eventArticle.title} onChange={onChange} />
 
-                <div className="createTitle">이벤트 기간</div>
-                <input name="end_date" className="createInput" placeholder="이벤트 기간" value={eventArticle.end_time} onChange={onChange} />
-                <div id="uploadIMG">
-                    <div className="createTitle">커버 이미지</div>
-                    <div id="uploadBox" style={{ backgroundImage: `url(${imageSrc})`}} > </div>
-                    <input
-                        id="inputIMG"
-                        type="file"
-                        accept='image/*'
-                        name="img"
-                        onChange={(e) => { encodeFileToBase64(e.target.files[0]) }}
-                    />
+                    <div className="createTitle">이벤트 기간</div>
+                    <input name="end_date" className="createInput" placeholder="이벤트 기간" value={eventArticle.end_time} onChange={onChange} />
+
+                    <div id="uploadIMG">
+                        <div className="createTitle">커버 이미지</div>
+                        <div id="uploadBox" style={{ backgroundImage: `url(${imageSrc})` }} > </div>
+                        <input
+                            id="inputIMG"
+                            type="file"
+                            accept='image/*'
+                            name="image"
+                            onChange={(e) => { encodeFileToBase64(e.target.files[0]) }}
+                        />
+                    </div>
                 </div>
- 
-            </div>
-            <div id="eventText" >
-                <textarea name="content" id="createText" placeholder="이벤트 내용" value={eventArticle.content} onChange={onChange} />
-            </div>
+                <div id="eventText" >
+                    <textarea name="content" id="createText" placeholder="이벤트 내용" value={eventArticle.content} onChange={onChange} />
+                </div>
+                <input
+                    type="file"
+                    accept='image/*'
+                    name="image" />
+          
 
-            <a className="reg" href="javascript:return false;" onClick={onClick} > 이벤트 주최하기 </a>
+                <button className= "reg" onClick={onClick} type="submit">이벤트 주최하기</button>
+            </form>
+
+
 
         </div>
     )
--- develuv-f/src/Containers/EventCreateContainer.js
+++ develuv-f/src/Containers/EventCreateContainer.js
@@ -4,13 +4,13 @@ import { useState } from 'react';
 import EventCreateContent from '../Components/EventCreate/EventCreateContent'
 import './EventCreateContainer.scss'
 import axios from 'axios'
+
 const EventCreateContainer = () => {
-    const [nullCheck, setnullCheck] = useState(false)
+    // const [nullCheck, setnullCheck] = useState(false)
     const [eventArticle, setEventArticle] = useState({
         user_id: 1,
         email: "chh123@naver.com",
         title: "",
-        img: "",
         onoff: null,
         type: null,
         end_date: "",
@@ -26,27 +26,25 @@ const EventCreateContainer = () => {
             [name]: value
         });
     };
-    
+    const urlMove = () => { window.location.href = '/' }
+
     const onClick = async () => {
-        // 빈값 체크
-        for (const i in eventArticle) {
-            if (eventArticle[i] !== null || eventArticle[i] !== "") {
-                setnullCheck(true)
-                break;
-            }
-        }
-        if (true) {
-            try {
-                await axios.post('http://localhost:8081/EventCreate',
-                    eventArticle
-                )
-            } catch (err) {
+
+        try {
+            await axios.post('http://localhost:8081/EventCreate',
+                eventArticle,
+            ).then((response) => {
+                alert('등록이완료 되었습니다')
+                window.location.href = '/'
+            }).catch((err)=>{
                 console.log(err)
-            }
-        } else {
-            alert('빈칸을 모두 입력하세요')
+            })
+        } catch (err) {
+            console.log(err)
         }
     }
+
+
 
     return (
         <div id="EventCreateContainer">
