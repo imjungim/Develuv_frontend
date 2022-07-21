1f5cf2f687c380ade8348d82263837935425d48a
jung im <jungim4201@gamil.com>
2022-07-22 00:32:01 +0900

    EventBanner.js image modify

--- develuv-f/src/Components/Explore/EventSearch.js
+++ develuv-f/src/Components/Explore/EventSearch.js
@@ -1,42 +1,36 @@
-import React,{useState, useRef} from "react";
-import { useSearchParams ,useNavigate} from "react-router-dom";
+import React, { useState, useRef } from "react";
+import { useSearchParams, useNavigate } from "react-router-dom";
 import "./EventSearch.scss";
 
 const EventSearch = () => {
-
-
   const navigate = useNavigate();
   const textInput = useRef();
   const [state, setState] = useState({
-    text : "",
-    type : "2",
+    text: "",
+    type: "2",
   });
- // console.log(state);
+  // console.log(state);
 
   const changeState = (e) => {
     setState({
       ...state,
-      [e.target.name] : e.target.value,
+      [e.target.name]: e.target.value,
     });
-  }
+  };
 
-  const getSearch = () => {    
-    navigate(`/search?keyword=${state.text}&type=${state.type}`)
-  }
+  const getSearch = () => {
+    navigate(`/search?keyword=${state.text}&type=${state.type}`);
+  };
 
-  const handleKeyPress = e => {
-    if(e.key === 'Enter') {
- 
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
       getSearch();
-      setState({
-        text : "",
-        type : "2",
-      })
     }
-  }
-  
-  const handleSubmit = () =>{
-    if(state.text.length <1 ){
+ 
+  };
+
+  const handleSubmit = () => {
+    if (state.text.length < 1) {
       //focus
       textInput.current.focus();
       return;
@@ -44,54 +38,48 @@ const EventSearch = () => {
 
     getSearch();
     setState({
-      text : "",
-      type : "2",
-    })
-  }
-
-  //검색기능
-
-
+      text: "",
+      type: "2",
+    });
+  };
+  console.log("state.text :", state.text);
   return (
-    <div className="EventList">
-
-      <section className="searchbar">
-        <div className="search-tabs">
-          <div className="search-name">
-            <form action="" method="GET" className="Search-input">
-              <div>검색어</div>
-              <input
-                ref={textInput}
-                name="text" 
-                onChange={changeState}
-                value={state.text}
-                onKeyPress={handleKeyPress}
-                placeholder="이벤트제목을 입력하세요"
-              />
-            </form>
-          </div>
-          <div className="search-type">
-            <div>이벤트 유형</div>
-            <select 
-              name="type"
-              value={state.type} 
-              className="select-event-type" 
-              onChange={changeState}>
-              <option value={2}>전체</option>
-              <option value={0}>온라인</option>
-              <option value={1}>오프라인</option>
-              
-            </select>
-          </div>
-          <div className="search-button">
-            <button 
-            type="submit"
-            onClick={handleSubmit}
-            >검색</button>
-
-          </div>
+    <div className="search-tabs">
+      <form
+        action=""
+        method="GET"
+        className="search-form"
+        onKeyDown={handleKeyPress}
+      >
+        <div className="search-name">
+          <div>검색어</div>
+          <input
+            ref={textInput}
+            name="text"
+            onChange={changeState}
+            value={state.text}
+            placeholder="이벤트제목을 입력하세요"
+          />
         </div>
-      </section>
+        <div className="search-type">
+          <div>이벤트 유형</div>
+          <select
+            name="type"
+            value={state.type}
+            className="select-event-type"
+            onChange={changeState}
+          >
+            <option value={2}>전체</option>
+            <option value={0}>온라인</option>
+            <option value={1}>오프라인</option>
+          </select>
+        </div>
+      </form>
+      <div className="search-button">
+        <button type="submit" onClick={handleSubmit}>
+          검색
+        </button>
+      </div>
     </div>
   );
 };
