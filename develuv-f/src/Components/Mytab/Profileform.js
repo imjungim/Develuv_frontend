193efaf1f46f276123ded2e949b5215e2cdcf0fb
Kimseonghyoek <cilo1223@naver.com>
2022-07-23 15:36:18 +0900

    Update UI in my info page

--- develuv-f/src/Components/Mytab/Profileform.js
+++ develuv-f/src/Components/Mytab/Profileform.js
@@ -1,62 +1,67 @@
 import "./scss/Profileform.scss";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import InputEmail from "./InputEmail";
 
 const Profileform = () => {
+  const [email, setEmail] = useState("none");
+  const [name, setName] = useState("none");
+  const [pw, setPw] = useState("none");
+  const [rePw, setRePw] = useState("none");
+  const [check, setCheck] = useState(true);
+
+  const checkBox = () => {
+    setCheck(!check);
+  }
+
   return (
     <div className="profile">
       <section className="profile-info">
-        <h1>기본정보</h1>
-        <div>
+        <div className="top_menu">
+          <h1>계정 정보</h1>
+          <div id="spaceBtnz" className="profile-button">
+            <button id="cancelBtn">취소</button>
+            <button>저장</button>
+          </div>
+        </div>
+        <hr width="750px" />
+        <div className="basic_info">
           <div className="profile-email">
             <h4>이메일</h4>
-            <p>aaaa@gmail.com</p>
+            <p>{email}</p>
           </div>
           <form name="profile" action="post">
             <div className="profileName">
-            <div className="input-name">
-              <label>성</label>
-              <input name="lastName" type="text" />
-            </div>
-            <div className="input-name">
-              <label>이름</label>
-              <input name="firstName" type="text"  />
-            </div>
-            </div>
-            <div className="input-name">
-              <label>전화번호</label>
-              <input name="phone" type="text"  />
-            </div>
-            <div className="profile-button">
-              <button type="submit">수정하기</button>
+              <div className="input-name">
+                <h4>닉네임</h4>
+                <input name="lastName" type="text" value={name} />
+              </div>
             </div>
           </form>
         </div>
       </section>
 
       <section className="profile-password">
-        <h1>비밀번호 변경</h1>
-        <div>
-          <form>
-            <div className="password-text">
-              <label>새로운 비밀번호</label>
-              <input name="newPassword" type="password" />
-            </div>
-            <div className="password-text">
-              <label>비밀번호 재입력</label>
-              <input name="confirmPassword" type="password" />
-            </div>
-            <div className="profile-button">
-              <button type="submit">변경하기</button>
-            </div>
-          </form>
-        </div>
+        <h4>비밀번호 변경</h4>
+        <form>
+          <div className="password-text">
+            <label>새로운 비밀번호</label>
+            <input name="newPassword" type="password" value={pw} />
+          </div>
+          <div className="password-text">
+            <label>비밀번호 재입력</label>
+            <input name="confirmPassword" type="password" value={rePw} />
+          </div>
+        </form>
       </section>
-      <section className="unregister" >
+      <section className="unregister">
         <div className="unregister">
-          <h2>회원탈퇴</h2>
-          <Link to="/unregister">
-          <button type="submit">탈퇴요청</button>
-          </Link>
+          <h4>회원탈퇴</h4>
+          <div className="check">
+            <input type="checkbox" onChange={checkBox} checked={check}></input>
+            <p>탈퇴에 동의하시나요?</p>
+          </div>
+          <InputEmail check={check} email={email}/>
         </div>
       </section>
     </div>
